e1aabd7f7ff12b65075f8c5189c7a8da3d312d95
Tanvinder <117674604+Tanvinder@users.noreply.github.com>
2022-11-07 19:48:19 -0500

    Add files via upload

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+
+	
+	public void makeCall()
+	{
+		System.out.println("make a call");
+		 
+	}
+	
+	public void sendMsg()
+	{
+		System.out.println("send a msg");
+	}
+	public static void main(String[] args) {
+		Mobile mob = new Mobile();
+		mob.makeCall();
+		mob.sendMsg();
+			
+				
+			
+	}
+
+}
