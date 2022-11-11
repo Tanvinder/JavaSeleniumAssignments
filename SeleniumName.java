d47ef4f36004b6e4a956824415d8dd4ae0e762f8
Tanvinder <117674604+Tanvinder@users.noreply.github.com>
2022-11-10 22:01:47 -0500

    Add files via upload

---
+++ SeleniumName.java
@@ -0,0 +1,29 @@
+package JavaSelenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumName {
+
+	public static void main(String[] args) {
+		
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/newtours/register.php");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		
+		driver.findElement(By.name("city")).sendKeys("Brampton");
+		
+	
+	
+	}
+
+}
